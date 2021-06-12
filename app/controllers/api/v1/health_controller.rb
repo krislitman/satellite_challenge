aecfbb5a88f6be5d6c047651551e6ccf33e4c920
Kris Litman <kris.d.litman@gmail.com>
2021-06-12 16:05:21 -0400

    Added health facade

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::HealthController < ApplicationController
   def current
+    data = HealthFacade.new
     render json: { 'message': { 'error': 'no data to display'}}, status: 500
   end
 end
---
+++ app/facades/health_facade.rb
@@ -0,0 +1,3 @@
+class HealthFacade
+  
+end
